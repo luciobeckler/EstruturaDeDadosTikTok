341fb4636522b7b6a0369bb7f9fd1fa7869c61d5
Lucio_Beckler <luciobecklerp@gmail.com>
2024-04-02 09:27:35 -0300

    Commit teste

--- src/main/java/com/grupo2/Main.java
+++ src/main/java/com/grupo2/Main.java
@@ -2,7 +2,7 @@ package com.grupo2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
 
     }
 }
